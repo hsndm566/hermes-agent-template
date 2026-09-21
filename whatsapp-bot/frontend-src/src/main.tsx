fbc8fa02128916b328480c7659e1340625391670
Hasan Adam <hasanadam506@gmail.com>
2026-09-21 19:58:01 +0300

    demo: open dashboard without login screen

--- whatsapp-bot/frontend-src/src/main.tsx
+++ whatsapp-bot/frontend-src/src/main.tsx
@@ -4,20 +4,16 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { I18nProvider } from './lib/i18n'
 import { BusinessProvider } from './lib/BusinessContext'
-import { AuthGate } from './lib/AuthGate'
-import Login from './pages/Login'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <I18nProvider>
-      <AuthGate loginScreen={(onSuccess) => <Login onSuccess={onSuccess} />}>
-        <BusinessProvider>
-          <HashRouter>
-            <App />
-          </HashRouter>
-        </BusinessProvider>
-      </AuthGate>
+      <BusinessProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </BusinessProvider>
     </I18nProvider>
   </React.StrictMode>,
 )
