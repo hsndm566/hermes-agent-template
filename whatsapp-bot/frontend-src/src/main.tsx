9141905f8314b0041c2491d003c31a2ba6a7ffcd
Hasan Adam <hasanadam506@gmail.com>
2026-09-21 20:31:09 +0300

    hardening: prevent blank dashboard failures

--- whatsapp-bot/frontend-src/src/main.tsx
+++ whatsapp-bot/frontend-src/src/main.tsx
@@ -4,16 +4,19 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { I18nProvider } from './lib/i18n'
 import { BusinessProvider } from './lib/BusinessContext'
+import { ErrorBoundary } from './components/ErrorBoundary'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <I18nProvider>
-      <BusinessProvider>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </BusinessProvider>
-    </I18nProvider>
+    <ErrorBoundary>
+      <HashRouter>
+        <I18nProvider>
+          <BusinessProvider>
+            <App />
+          </BusinessProvider>
+        </I18nProvider>
+      </HashRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
